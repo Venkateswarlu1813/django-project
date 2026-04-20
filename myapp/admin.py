8ddfa6393824f8d32794c400826458193db7f054
Venkateswarlu1813 <venkateswarlu4466@gmail.com>
2026-04-20 15:53:04 +0530

    Completed Django CRUD APIs, admin panel, Postman testing, dynamic URLs

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here.from django.contrib import admin
+from .models import Course, Student
+
+admin.site.register(Course)
+admin.site.register(Student)
